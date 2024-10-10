5e0f0efc536b144b2d8865d84ed96c81487527cd
SonaBadalian <153511333+SonaBadalian@users.noreply.github.com>
2024-10-10 06:45:52 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Sorting
+{
+    public static class SortingAlgorithms
+    {
+        public static void InsertionSortRecursive(int[] array)
+        {
+            InsertionSortRecursiveHelper(array, array.Length);
+        }
+
+        private static void InsertionSortRecursiveHelper(int[] array, int n)
+        {
+            if (n <= 1)
+                return;
+
+            InsertionSortRecursiveHelper(array, n - 1);
+
+            int last = array[n - 1];
+            int j = n - 2;
+
+            while (j >= 0 && array[j] > last)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = last;
+        }
+    }
+}
